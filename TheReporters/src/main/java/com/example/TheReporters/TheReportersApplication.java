fffec90cb567d259db7e62d7aed1973493c9f805
Annica Landbü <annica.landbu@hm.com>
2021-05-07 15:24:23 +0200

    Original code

---
+++ TheReporters/src/main/java/com/example/TheReporters/TheReportersApplication.java
@@ -0,0 +1,13 @@
+package com.example.TheReporters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheReportersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheReportersApplication.class, args);
+	}
+
+}
